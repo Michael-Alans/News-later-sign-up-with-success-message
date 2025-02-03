690fb04abaaa3c96c4cb80859d39ee2417b84a76
alanememichael9@gmail.com <alanememichael9@gmail.com>
2025-02-03 23:53:16 +0100

    updated js form validation and css responsiveness

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const container = document.querySelector('.container');
 const successMessage = document.querySelector('.success-message');
 const submit = document.getElementById('submit');
-const reset = document.querySelector('span')
+const reset = document.getElementById('second-span')
 const dismissMessage = document.getElementById('Dismiss');
+const form = document.getElementById('form');
+const emailField = document.getElementById('Email');
+const error = document.getElementById('email-error-message')
+
 
 
 
@@ -11,7 +15,7 @@ const dismissMessage = document.getElementById('Dismiss');
 
 
 //Event listeners 
-submit.addEventListener('click', function() {
+/*submit.addEventListener('click', function() {
 
     const validateForm = document.forms['Email-form']['Email'].value;
 
@@ -31,5 +35,23 @@ dismissMessage.addEventListener('click', function() {
     successMessage.style.display ="none";
     
         
-})
+})*/
 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const EmailInputValue = emailField.value;
+
+    if(EmailInputValue.includes('@')) {
+        container.style.display ="none";
+        successMessage.style.display ="block";
+
+        reset.innerText = ` ${EmailInputValue} `
+    } else if (EmailInputValue === "") {
+        error.style.visibility = "visible";
+        emailField.style.borderColor = "red";
+        emailField.style.backgroundColor = "rgba(255, 0, 0, 0.2)"
+        return false;
+    }
+    
+})
